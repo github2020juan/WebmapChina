098612b716e9ff61847320bf6981174411785fe4
github2020juan <95059076+github2020juan@users.noreply.github.com>
2021-12-23 01:33:07 -0300

    Add files via upload

---
+++ data/Puertos_6.js
@@ -0,0 +1,28 @@
+var json_Puertos_6 = {
+"type": "FeatureCollection",
+"name": "Puertos_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nombre": "Puerto de NINGBO" }, "geometry": { "type": "Point", "coordinates": [ 109.327933074374428, 16.415716717254323 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de CHANGSHU" }, "geometry": { "type": "Point", "coordinates": [ 109.327925050507034, 16.415732444863387 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de CHANGZHOU" }, "geometry": { "type": "Point", "coordinates": [ 109.327917652693543, 16.415733386061166 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de CHIWAN" }, "geometry": { "type": "Point", "coordinates": [ 109.327863490238101, 16.41564809000635 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de CHONGQING" }, "geometry": { "type": "Point", "coordinates": [ 109.327793792069698, 16.415711432978298 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de DA CHAN BAY" }, "geometry": { "type": "Point", "coordinates": [ 109.327863321508516, 16.415648536444358 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de DALIAN" }, "geometry": { "type": "Point", "coordinates": [ 109.327931099223008, 16.415798184758259 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de FOSHAN" }, "geometry": { "type": "Point", "coordinates": [ 109.327856309478122, 16.415652723007838 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de FOSHAN NEW PORT" }, "geometry": { "type": "Point", "coordinates": [ 109.327855717391529, 16.415652265741826 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de FUZHOU" }, "geometry": { "type": "Point", "coordinates": [ 109.327913160897666, 16.415681712270203 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de GUANGZHOU" }, "geometry": { "type": "Point", "coordinates": [ 109.327857392074961, 16.415653564826645 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de HAIKOU" }, "geometry": { "type": "Point", "coordinates": [ 109.327831062800755, 16.415625110285095 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de HUANGPU" }, "geometry": { "type": "Point", "coordinates": [ 109.327859185501339, 16.415653558600333 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de HUANGPU OLD PORT" }, "geometry": { "type": "Point", "coordinates": [ 109.327906780220886, 16.415746771389756 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de ZHUHAI" }, "geometry": { "type": "Point", "coordinates": [ 109.327860587351211, 16.415646203259943 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de ZHONGSHAN" }, "geometry": { "type": "Point", "coordinates": [ 109.327832869147585, 16.415636658685695 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de ZHENJIANG" }, "geometry": { "type": "Point", "coordinates": [ 109.327912872459237, 16.415737197058704 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de YICHANG" }, "geometry": { "type": "Point", "coordinates": [ 109.327836957419791, 16.415721488414647 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de YANTIAN" }, "geometry": { "type": "Point", "coordinates": [ 109.327918191358478, 16.415689039011411 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de NANJING" }, "geometry": { "type": "Point", "coordinates": [ 109.327906586197471, 16.415735673318302 ] } },
+{ "type": "Feature", "properties": { "nombre": "Puerto de SHÁNGHAI" }, "geometry": { "type": "Point", "coordinates": [ 109.327931853461806, 16.415728811342476 ] } }
+]
+}
