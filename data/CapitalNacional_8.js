7ce1e76edd7347397e4f6ce0d6a35b58eeffacf1
github2020juan <95059076+github2020juan@users.noreply.github.com>
2021-12-23 01:03:02 -0300

    Add files via upload

---
+++ data/CapitalNacional_8.js
@@ -0,0 +1,8 @@
+var json_CapitalNacional_8 = {
+"type": "FeatureCollection",
+"name": "CapitalNacional_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Capital Na": "Beijing", "Fundacion ": "1 de Octubre de 1949", "Población": "1.411.000.000 Habitantes", "Idioma": "Mandarín Estandar Moderno (Putonghua)", "Moneda": "Renminbi (RMB)  - \"Moneda del Pueblo\"" }, "geometry": { "type": "Point", "coordinates": [ 109.327882204700046, 16.415805786001442 ] } }
+]
+}
